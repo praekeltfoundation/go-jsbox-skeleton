fedd27576ede3ad32c77720bd9daa677c325c091
Simon Cross <hodgestar@gmail.com>
2015-08-04 15:39:09 +0200

    Revert "go-jsbox-skeleton"
    This reverts commit f36d9cb9d4ffbbb5e895d598d4c9a058dc525f05.

--- test/app.test.js
+++ test/app.test.js
@@ -30,7 +30,7 @@ describe("app", function() {
                         state: 'states:start',
                         reply: [
                             'Hi there! What do you want to do?',
-                            '1. Search for music',
+                            '1. Show this menu again',
                             '2. Exit'
                         ].join('\n')
                     })
@@ -47,7 +47,7 @@ describe("app", function() {
                         state: 'states:start',
                         reply: [
                             'Hi there! What do you want to do?',
-                            '1. Search for music',
+                            '1. Show this menu again',
                             '2. Exit'
                         ].join('\n')
                     })
@@ -62,7 +62,7 @@ describe("app", function() {
                     .input('2')
                     .check.interaction({
                         state: 'states:end',
-                        reply: 'As long as it\'s good music, that\'s what\'s important'
+                        reply: 'Thanks, cheers!'
                     })
                     .check.reply.ends_session()
                     .run();
